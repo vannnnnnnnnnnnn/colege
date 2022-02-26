8e7b5c3bda78ae99424eb0a74f9b3743c208c887
van1 <99073835+vannnnnnnnnnnnn@users.noreply.github.com>
2022-02-26 12:55:38 +0300

    Add files via upload

---
+++ ITCollege/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ITCollege.Pages;
+
+namespace ITCollege
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           // Frame0.NavigationService.Navigate(new UserPage());
+        }
+
+        private void AuthBT_Click(object sender, RoutedEventArgs e)
+        {
+            Frame1.Navigate(new AuthPage());
+            AuthBT.Visibility = Visibility.Hidden;
+        }
+    }
+}
---
+++ ITCollege/Pages/AuthPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ITCollege.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SignInBT_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new UserPage());
+
+        }
+    }
+}
